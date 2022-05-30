59ca26c3db99e1ae38a0a9ba79e87c516b3b04b2
SanSciain <mastroeni.santi@gmail.com>
2022-05-30 13:11:00 +0200

    Add: AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <AppHeader />
+    <AppHeader :menuItems="headerMenu" />
     <AppMain />
     <AppFooter />
   </div>
@@ -10,6 +10,14 @@
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 import AppFooter from "./components/AppFooter.vue";
+
+import Vue from "vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+library.add(faCartShopping);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 export default {
   name: "App",
   components: {
@@ -17,7 +25,63 @@ export default {
     AppMain,
     AppFooter,
   },
+  data() {
+    return {
+      headerMenu: [
+        {
+          name: "Home",
+          active: true,
+          isIcon: false,
+          new: false,
+        },
+        {
+          name: "Culinary History",
+          active: false,
+          isIcon: false,
+          new: false,
+        },
+        {
+          name: "Our Team",
+          active: false,
+          isIcon: false,
+          new: false,
+        },
+        {
+          name: "Our Menu",
+          active: false,
+          isIcon: false,
+          new: false,
+        },
+        {
+          name: "Takeout",
+          active: false,
+          isIcon: false,
+          new: true,
+        },
+        {
+          name: "Bulletin",
+          active: false,
+          isIcon: false,
+          new: false,
+        },
+        {
+          name: "Reservations",
+          active: false,
+          isIcon: false,
+          new: false,
+        },
+        {
+          name: "fa-solid fa-cart-shopping",
+          active: false,
+          isIcon: true,
+          new: false,
+        },
+      ],
+    };
+  },
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "./style/common.scss";
+</style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,78 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div class="row py-4">
+      <div class="col col-5">
+        <div class="ms-img-container">
+          <img src="../assets/img/logo-restaurant-2x-300x58.png" alt="logo" />
+        </div>
+      </div>
+      <div class="col col-7">
+        <ul>
+          <li
+            v-for="(item, index) in menuItems"
+            :key="index"
+            :class="{ isActive: item.active }"
+          >
+            <span v-if="item.isIcon">
+              <font-awesome-icon :icon="`fa-solid ${item.name}`"
+            /></span>
+            <span v-else>
+              {{ item.name }}
+            </span>
+            <span class="new" :class="{ isVisible: item.new }">New</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "AppHeader",
+  props: {
+    menuItems: Array,
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container-fluid {
+  position: fixed;
+}
+.ms-img-container {
+  width: 300px;
+}
+
+ul {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+
+  li {
+    color: #bcbcbc;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 0.8rem;
+    font-weight: 700;
+    &.isActive {
+      color: white;
+    }
+
+    .new {
+      display: none;
+      padding: 0.1rem 0.3rem;
+      font-size: 0.5rem;
+      border: 1px solid silver;
+      line-height: 0.7rem;
+      color: silver;
+      vertical-align: middle;
+
+      &.isVisible {
+        display: inline-block;
+      }
+    }
+  }
+}
+</style>
